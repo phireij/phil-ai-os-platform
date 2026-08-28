f8bf423fba9733f86aa1e9a5f6cc9bca37927c87
phireij <phireij@gmail.com>
2026-08-28 21:58:42 +0900

    sprint6: export dry-run boundary request

--- apps/automation-hub/src/automation_hub/__init__.py
+++ apps/automation-hub/src/automation_hub/__init__.py
@@ -1,4 +1,5 @@
 from .approval import ApprovalReplayError, ApprovalSimulationError, ApprovalSimulationStore
+from .boundary import BoundaryRequestError, build_dry_run_boundary_request
 from .planner import AutomationPlanError, build_automation_plan
 
 __all__ = [
@@ -6,5 +7,7 @@ __all__ = [
     "ApprovalSimulationError",
     "ApprovalSimulationStore",
     "AutomationPlanError",
+    "BoundaryRequestError",
     "build_automation_plan",
+    "build_dry_run_boundary_request",
 ]
