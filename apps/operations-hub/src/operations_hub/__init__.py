f43b179b2b84498fc2aa203a288cc0ec7017f8fc
phireij <phireij@gmail.com>
2026-09-11 16:19:07 +0900

    Sprint 5: export order intake handoff normalizer

--- apps/operations-hub/src/operations_hub/__init__.py
+++ apps/operations-hub/src/operations_hub/__init__.py
@@ -7,6 +7,7 @@ from .normalizer import (
     classify_intent,
     normalize_channel_event,
 )
+from .order_intake import OrderIntakeHandoffError, normalize_order_intake_handoff
 from .queue import OperationsQueue
 
 __all__ = [
@@ -17,9 +18,11 @@ __all__ = [
     "MockChannelAdapter",
     "NormalizationError",
     "OperationsQueue",
+    "OrderIntakeHandoffError",
     "SUPPORTED_SOURCES",
     "classify_intent",
     "evaluate_governance",
     "normalize_channel_event",
+    "normalize_order_intake_handoff",
     "retry_decision",
 ]
